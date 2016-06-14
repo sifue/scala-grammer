8bf2bc7e847ebb69f0d410853d8d5f6f395f463b
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-06-14 17:42:08 +0900

    攻撃と逃走の実装

--- src/main/scala-2.11/RPG.scala
+++ src/main/scala-2.11/RPG.scala
@@ -1,7 +1,10 @@
 /**
   * Created by soichiro_yoshimura on 2016/06/13.
   */
+import java.util.Random
+
 object RPG {
+  val random = new Random
 
 }
 
@@ -11,17 +14,28 @@ abstract class Creature(var hitPoint: Int, var attackDamage: Int) {
 
 class Hero(_hitPoint: Int, _attackDamage: Int) extends Creature(_hitPoint, _attackDamage) {
 
-  def attack(monster: Monster): Unit = ???
+  def attack(monster: Monster): Unit = {
+    monster.hitPoint = monster.hitPoint - this.attackDamage
+    this.hitPoint = this.hitPoint - monster.attackDamage
+  }
 
-  def escape(monster: Monster): Boolean = ???
+  def escape(monster: Monster): Boolean = {
+    val isEscape = RPG.random.nextInt(2) == 1
+    if (!isEscape) {
+      this.hitPoint = this.hitPoint - monster.attackDamage
+    } else {
+      monster.isAwayFromHero = true
+    }
+    isEscape
+  }
 
-  override def toString = s"Hero(体力: ${hitPoint}, 攻撃力:${attackDamage})"
+  override def toString = s"Hero(体力:${hitPoint}, 攻撃力:${attackDamage})"
 
 }
 
 class Monster(_hitPoint: Int, _attackDamage: Int, var isAwayFromHero: Boolean)
   extends  Creature(_hitPoint, _attackDamage) {
 
-  override def toString = s"Monster(体力: ${hitPoint}, 攻撃力:${attackDamage})"
+  override def toString = s"Monster(体力: ${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
 
 }
