23a88c3338e7570816221e687607e1ceb5c67439
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-06-27 19:16:28 +0900

    13節まで

--- src/main/scala-2.11/ShortestPath.scala
+++ src/main/scala-2.11/ShortestPath.scala
@@ -1,40 +1,59 @@
 /**
-  * Created by soichiro_yoshimura on 2016/06/22.
+  * Created by soichiro_yoshimura on 2016/06/27.
   */
-case class Edge(from: Char, to: Char, cost: Int)
+case class Edge(from: Char, to: Char, distance: Int)
 
 object ShortestPath {
+
   /**
     * 頂点
     */
-  val vertexes = 'A' to 'G'
+  val vertexes = 'A' to 'N'
 
   /**
     * 辺
     */
   val edges = Seq(
-    Edge('A', 'B', 1),
-    Edge('B', 'A', 1),
-    Edge('A', 'C', 8),
-    Edge('C', 'A', 8),
-    Edge('C', 'B', 6),
-    Edge('B', 'C', 6),
-    Edge('C', 'D', 7),
-    Edge('D', 'C', 7),
-    Edge('B', 'D', 6),
-    Edge('D', 'B', 6),
-    Edge('B', 'E', 6),
-    Edge('E', 'B', 6),
-    Edge('D', 'F', 2),
-    Edge('F', 'D', 2),
-    Edge('E', 'F', 6),
-    Edge('F', 'E', 6),
-    Edge('E', 'G', 8),
-    Edge('G', 'E', 8),
-    Edge('F', 'G', 5),
-    Edge('G', 'F', 5)
+    Edge('A', 'B', 9),
+    Edge('A', 'C', 6),
+    Edge('A', 'D', 6),
+    Edge('B', 'A', 9),
+    Edge('B', 'E', 2),
+    Edge('C', 'A', 6),
+    Edge('C', 'E', 9),
+    Edge('C', 'G', 6),
+    Edge('D', 'A', 6),
+    Edge('D', 'F', 3),
+    Edge('E', 'B', 2),
+    Edge('E', 'C', 9),
+    Edge('E', 'I', 1),
+    Edge('F', 'D', 3),
+    Edge('F', 'H', 5),
+    Edge('F', 'J', 9),
+    Edge('G', 'C', 6),
+    Edge('G', 'I', 3),
+    Edge('G', 'J', 9),
+    Edge('H', 'F', 5),
+    Edge('H', 'K', 5),
+    Edge('I', 'E', 1),
+    Edge('I', 'G', 3),
+    Edge('J', 'F', 9),
+    Edge('J', 'G', 9),
+    Edge('J', 'K', 4),
+    Edge('J', 'L', 7),
+    Edge('J', 'M', 6),
+    Edge('K', 'H', 5),
+    Edge('K', 'J', 4),
+    Edge('K', 'M', 1),
+    Edge('L', 'J', 7),
+    Edge('L', 'N', 3),
+    Edge('M', 'J', 6),
+    Edge('M', 'K', 1),
+    Edge('M', 'N', 2),
+    Edge('N', 'L', 3),
+    Edge('N', 'M', 2)
   )
-  
+
   def solveByBellmanFord(start: Char, goal: Char): Unit = {
     // 各頂点までの距離の初期化
     var distances = vertexes.map(v => (v -> Int.MaxValue)).toMap
@@ -45,8 +64,8 @@ object ShortestPath {
       isUpdated = false
       edges.foreach { e =>
         if(distances(e.from) != Int.MaxValue
-          && distances(e.to) > distances(e.from) + e.cost) {
-          distances = distances + (e.to -> (distances(e.from) + e.cost))
+          && distances(e.to) > distances(e.from) + e.distance) {
+          distances = distances + (e.to -> (distances(e.from) + e.distance))
           isUpdated = true
         }
       }
@@ -69,8 +88,8 @@ object ShortestPath {
       edges.foreach { e =>
         if(!usedEdges.contains(e)
           && distances(e.from) != Int.MaxValue
-          && distances(e.to) > distances(e.from) + e.cost) {
-          distances = distances + (e.to -> (distances(e.from) + e.cost))
+          && distances(e.to) > distances(e.from) + e.distance) {
+          distances = distances + (e.to -> (distances(e.from) + e.distance))
           usedEdges = usedEdges + e
           isUpdated = true
         }
@@ -84,7 +103,7 @@ object ShortestPath {
   def solveByWarshallFloyd(start: Char, goal: Char): Unit = {
     // 二頂点間の距離の初期化
     var distanceMap: Map[(Char, Char), Int] = vertexes.map(v => ((v , v) -> 0)).toMap
-    distanceMap = distanceMap ++ edges.map(e => (e.from, e.to) -> e.cost)
+    distanceMap = distanceMap ++ edges.map(e => (e.from, e.to) -> e.distance)
 
     def distance(v1: Char, v2: Char): Int = distanceMap.getOrElse((v1, v2), Int.MaxValue / 2)
     for (v1 <- vertexes; v2 <- vertexes; v3 <- vertexes) {
