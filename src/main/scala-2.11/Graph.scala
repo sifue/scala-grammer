75ff8d828a620c14b492898463e327205e2c8faa
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-06-30 17:32:41 +0900

    グラフの記述の変更

--- src/main/scala-2.11/Graph.scala
+++ src/main/scala-2.11/Graph.scala
@@ -11,21 +11,28 @@ object Graph {
       Array(0, 0, 1, 1, 0)
     )
 
+  val list: Map[Char, Seq[Char]] = Map(
+    'A' -> Seq('B', 'C'),
+    'B' -> Seq('A', 'D'),
+    'C' -> Seq('A', 'D', 'E'),
+    'D' -> Seq('B', 'D', 'E'),
+    'E' -> Seq('C', 'D')
+  )
+  
   case class Edge(from: Char, to: Char)
 
-  val list = Seq(
-      Edge('A', 'B'),
-      Edge('B', 'A'),
-      Edge('A', 'C'),
-      Edge('C', 'A'),
-      Edge('B', 'D'),
-      Edge('D', 'B'),
-      Edge('C', 'D'),
-      Edge('D', 'D'),
-      Edge('C', 'E'),
-      Edge('E', 'C'),
-      Edge('D', 'E'),
-      Edge('E', 'D')
+  val edges = Seq(
+    Edge('A', 'B'),
+    Edge('A', 'C'),
+    Edge('B', 'A'),
+    Edge('B', 'D'),
+    Edge('C', 'A'),
+    Edge('C', 'D'),
+    Edge('C', 'E'),
+    Edge('D', 'B'),
+    Edge('D', 'D'),
+    Edge('D', 'E'),
+    Edge('E', 'C'),
+    Edge('E', 'D')
   )
-
 }
